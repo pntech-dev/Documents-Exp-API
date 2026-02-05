36cf740a53b5abecad4faedd491640607c4ffcb7
PN Tech <pavlopavlo20055002@gmail.com>
2026-02-05 14:18:10 +0300

    chore(config): add dockerfiles and update code for production environment

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from routers import AuthRouter, AppRouter
 
 
-app = FastAPI(title="Documents Exp API")
+app = FastAPI(
+    title="Documents Exp API",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
 
 app.include_router(AuthRouter)
 app.include_router(AppRouter)
@@ -12,7 +17,7 @@ app.include_router(AppRouter)
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=False
     )
